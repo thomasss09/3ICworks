5efb109673a1793e8cfee3af70c4f305ac849eec
thomasss09 <thomasflash21@gmail.com>
2026-01-30 06:20:45 +0100

    commit

--- Gennaio/Gennaio26/Target.java
+++ Gennaio/Gennaio26/Target.java
@@ -2,37 +2,8 @@
 public enum Target {
     ATK_FORTE, ATK_DEBOLE, DEF_FORTE, DEF_DEBOLE, HP_ALTO, HP_BASSO;
 
-    public static int creaTarget() {
+    public static Target creaTarget() {
         int rand = (int) (Math.random() * (6 + 0) - 0);
-        Target random = Target.values()[rand];
-         
-        Target s = Target.values()[rand];
-        /*switch (random) {
-            /*case : {
-                random = ATK_FORTE;
-                break;
-            }
-
-            case ATK_DEBOLE:
-                break;
-
-            case DEF_FORTE:
-                break;
-
-            case DEF_DEBOLE:
-                break;
-
-            case HP_ALTO:
-                break;
-
-            case HP_BASSO:
-                break;
-
-            default:
-                throw new IllegalStateException("Stato non valido: " + rand);
-        }*/
-
-        System.out.println("il Target scelto è ");
-        return 0;
+        return  Target.values()[rand];
     }
 }
