3fed2a1ccade88524105c58a16a888b5a71e42ad
Thomas Guzzo <thomasflash21@gmail.com>
2026-02-14 11:48:19 +0100

    Update Cerchio.java

--- Febbraio/ASCIICanvas/Cerchio.java
+++ Febbraio/ASCIICanvas/Cerchio.java
@@ -8,18 +8,22 @@ public class Cerchio implements Forma {
         this.centroX = centroX;
         this.centroY = centroY;
         this.raggio = raggio;
-        this.carattere = carattere;
+        this.carattere = carattere = #;
     }
-    @Override
+    @Override(x-cx)² + (y - cy)²
     public boolean contiene(double x, double y) { // condizione che scrive prof if vera true
+		cs = cx+x;
+		ccs = cy+y;
+				if(((Math.pow(x , 2) + (Math.pow(cx , 2)) + (Math.pow(cs , 2))) + ((Math.pow(x , 2) + (Math.pow(cx , 2)) + (Math.pow(ccs , 2)) ≤ Math.pow(raggio , 2)){
+			return true;
+				}
         // TODO Auto-generated method stub
+else
         return false;
     }
     
     @Override
-    public char getCarattere() { // return carattere
-        // TODO Auto-generated method stub
-        return 0;
+    public char getCarattere(char carattere) { // return carattere
+        return carattere;
     }
-    
-    }
+}
